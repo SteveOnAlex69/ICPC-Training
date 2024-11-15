b1005b9e78ae9f2fb48a45b20f14677779e63b6e
tinpc007 <ngohuytin007.qn@gmail.com>
2024-11-16 00:06:11 +0700

    Them code Geometry!

---
+++ Teamnote/Geometry (Dash)/Point_In_Poly.cpp
@@ -0,0 +1,19 @@
+/* 
+ * Usage : 
+ * Returns true if p lies within the polygon. If strict is true,
+ * it returns false for points on the boundary.
+ * The algorithm uses products in intermediate steps so watch out for overflow.
+ * Time complexity : O(n)
+*/
+
+template<class P>
+bool inPolygon(vector<P> &p, P a, bool strict = true) {
+	int cnt = 0, n = sz(p);
+	rep(i,0,n) {
+		P q = p[(i + 1) % n];
+		if (onSegment(p[i], q, a)) return !strict;
+		//or: if (segDist(p[i], q, a) <= eps) return !strict;
+		cnt ^= ((a.y<p[i].y) - (a.y<q.y)) * a.cross(p[i], q) > 0;
+	}
+	return cnt;
+}
